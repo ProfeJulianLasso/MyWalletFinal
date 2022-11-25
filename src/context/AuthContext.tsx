a5d3ca622edaad3eeb26d7524ea23f95c28ed16e
Julian Lasso <98534497+ProfeJulianLasso@users.noreply.github.com>
2022-11-25 10:39:40 -0500

    Toda la parte social se encuentra trabajando perfectamente, se agregan comentarios

--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -21,8 +21,12 @@ const AuthContextProvider = (props: any) => {
   >();
 
   const getUserData = async (id?: string) => {
+    // Saco el token del SInfo, algo que deberíamos cambiar por el Redux
     const idToken = id ? id : await SInfo.getItem('idToken', {});
+    // Decodifico el token (JWT)
     const { name, picture, exp } = jwtDecode<any>(idToken);
+    const data = jwtDecode<any>(idToken);
+    console.log('data JWT', JSON.stringify(data, null, 2));
 
     if (exp < Date.now() / 1000) {
       throw new Error('ID token expired!');
@@ -69,7 +73,9 @@ const AuthContextProvider = (props: any) => {
       const credentials = await auth0.webAuth.authorize({
         scope: 'openid email profile',
       });
+      // Aquí se obtiene el token JWT
       await SInfo.setItem('idToken', credentials.idToken, {});
+      // Se obtiene la información del usuario
       const user_data = await getUserData(credentials.idToken);
       setLoggedIn(true);
       setUserData(user_data);
@@ -80,11 +86,14 @@ const AuthContextProvider = (props: any) => {
 
   const logout = async () => {
     try {
-      await auth0.webAuth.clearSession({});
+      // Limpiando la sesión en Auth0
+      await auth0.webAuth.clearSession();
+      // Limpiando la sesión. En nuestro caso sería en el Redux
       await SInfo.deleteItem('idToken', {});
       setLoggedIn(false);
       setUserData(undefined);
     } catch (err) {
+      console.log(err);
       Alert.alert('Error logging in');
     }
   };
